916eadbaeddce1a9661acecb19b0037b9de9f242
Vitor Hugo Pereira <vitorhugopereirasilva12@gmail.com>
2025-10-29 10:10:00 -0300

    Questao2

---
+++ Questao2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Questao2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite um número: ");
+        int n = sc.nextInt();
+        imprimirPadrao(n);
+        sc.close();
+    }
+
+    static void imprimirPadrao(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
